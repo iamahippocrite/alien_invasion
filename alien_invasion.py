02dd4f020ddb0469cb99201457c322428eba7eec
iamahippocrite <hk355750@gmail.com>
2023-12-06 22:05:49 +0530

    Added Movement to ship and added fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,28 +13,62 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-
+        self.fullscreen_status = 1
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height), pygame.RESIZABLE)
         pygame.display.set_caption('Alien Invasion')
         self.ship = Ship(self)
-        
 
     def run_game(self):
         '''Start the main loop of the game'''
         while True:
-            #Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type==pygame.QUIT:
-                    pygame.quit()
-                    sys.exit
-            
-            #Redraw the screen each pass of the loop
-            self.screen.fill(self.settings.bg_colour)
-            self.ship.blitme()
-
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()            
+            self._update_screen()
+            self.ship.update()
             self.clock.tick(60)
+    
+    def _check_events(self):
+        '''Responds to keypresses and mouse events'''
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)                    
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events (event)                       
+
+    def _check_keydown_events(self,event):
+        """ Respond to keypresses """                
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            pygame.quit()
+            sys.exit    
+        elif event.key == pygame.K_f:
+            self._toggle_fullscreen()
+    def _check_keyup_events(self, event):
+        """ Responding to keyreleases """
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _toggle_fullscreen(self):
+        self.fullscreen_status *= -1
+        if self.fullscreen_status == 1:
+            self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height), pygame.RESIZABLE)
+        if self.fullscreen_status == -1:
+            self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+            self.settings.screen_width = self.screen.get_rect().width
+            self.settings.screen_height = self.screen.get_rect().height
+
+    def _update_screen(self):
+        '''Update images on screen and flip to the new screen'''
+        self.screen.fill(self.settings.bg_colour)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@ class Settings:
     def __init__(self) -> None:
         '''Initialize the games settings'''
         #Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_colour = (230, 230, 230)
-        
+        self.screen_width = 1200 #Default is 1200
+        self.screen_height = 800 #Default is 800
+        self.bg_colour = (230, 230, 230) #Default is (230, 230, 230)
+
+        #Ship settings
+        self.ship_speed = 5 #Default is 1.5        
